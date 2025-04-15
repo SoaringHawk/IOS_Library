c0b6d4fede84f38ec42f4bac9b6b2cdf6a2929ae
YunxianXu <xuyunxian.12@gmail.com>
2025-04-15 01:43:16 -0400

    debug book shelves bg color

--- LibrarySystemV5/Model/MyBookShelfScreen.swift
+++ LibrarySystemV5/Model/MyBookShelfScreen.swift
@@ -17,56 +17,61 @@ struct MyBookShelfScreen: View {
         GridItem(.flexible())
     ]
     var body: some View {
-        VStack {
-            HStack{
-                Text("My books")
-                    .font(.title)
-                    .padding(.leading)
-                    .foregroundStyle(.black)
-                Spacer()
-            }
-            
-            
-            if firebaseManager.books.isEmpty {
+        NavigationStack {
+            VStack {
+                HStack{
+                    Text("My books")
+                        .font(.title)
+                        .padding(.leading)
+                        .foregroundStyle(.black)
+                    Spacer()
+                }
                 
-                Text("Loading books...")
-                    .padding()
-            } else {
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 20) {
-                        ForEach(firebaseManager.books, id: \.id) { book in
-                            if book.renter.contains(firebaseManager.loggedUser)  {
-                                if let url = URL(string: book.pdfLink), url != nil {
-                                    Button(action: {
-                                        selectedPdfURL = URL(string: book.pdfLink)
-                                        navigationActive = true
-                                    }) {
-                                        VStack {
-                                            BookGridView(book: book)
-                                            Text("Read Book")
-                                                .fontWeight(.bold)
-                                                .foregroundColor(.blue)
-                                                .padding(.top, 5)
+                
+                if firebaseManager.books.isEmpty {
+                    
+                    Text("Loading books...")
+                        .padding()
+                } else {
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 20) {
+                            ForEach(firebaseManager.books, id: \.id) { book in
+                                if book.renter.contains(firebaseManager.loggedUser)  {
+                                    if let url = URL(string: book.pdfLink), url != nil {
+                                        Button(action: {
+                                            selectedPdfURL = URL(string: book.pdfLink)
+                                            navigationActive = true
+                                        }) {
+                                            VStack {
+                                                BookGridView(book: book)
+                                                Text("Read Book")
+                                                    .fontWeight(.bold)
+                                                    .foregroundColor(.blue)
+                                                    .padding(.top, 5)
+                                            }
                                         }
-                                    }
-                                    .background(
-                                        NavigationLink(
-                                            destination: selectedPdfURL.map { Reader(url: $0) },
-                                            isActive: $navigationActive
-                                        ) {
-                                            EmptyView()
-                                        }
-                                        .hidden()
-                                    )                                }
+                                        .background(
+                                            NavigationLink(
+                                                destination: selectedPdfURL.map { Reader(url: $0) },
+                                                isActive: $navigationActive
+                                            ) {
+                                                EmptyView()
+                                            }
+                                            .hidden()
+                                        )                                }
+                                }
                             }
                         }
+                        .padding(.horizontal)
                     }
-                    .padding(.horizontal)
                 }
             }
+            .toolbarBackground(.visible, for: .navigationBar) //modify for bar
+            .onAppear {
+                firebaseManager.fetchBooks()// Ensure data is fetched when the view appears
         }
-        .onAppear {
-            firebaseManager.fetchBooks()  // Ensure data is fetched when the view appears
+
+
         }
     }
 }
